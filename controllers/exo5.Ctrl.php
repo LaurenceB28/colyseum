d06a4ea96c4d49b7d6bd376833ef79b1bcee1eff
LaurenceB28 <laurence.brd@gmail.com>
2023-05-26 10:57:05 +0200

    bv

--- controllers/exo5.Ctrl.php
+++ controllers/exo5.Ctrl.php
@@ -4,5 +4,5 @@ $clientMonly = clientsMOnly();
 
 
 include __DIR__ .'/../views/templates/header.php';
-include __DIR__ .'/../views/exos/exo6.php';
+include __DIR__ .'/../views/exos/exo5.php';
 include __DIR__ .'/../views/templates/footer.php';
